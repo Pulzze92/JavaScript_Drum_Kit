a4191b4d5b7eca1c034716e8866a8752e219adf4
Pulzze92 <89998310392@mail.ru>
2021-03-04 23:13:48 +0300

    feat: add style

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+ }
+
 function playSound(e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
@@ -7,3 +12,7 @@ function playSound(e) {
 	audio.currentTime = 0;
 	audio.play();
  }
+
+ const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
